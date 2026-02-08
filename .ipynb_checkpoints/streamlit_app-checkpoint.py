9fa71ebe8db6b8d647d50306d6b83b48298c834d
Debayan Raha <debayan.raha@gmail.com>
2026-02-09 01:01:04 +0530

    ML models + Streamlit app

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+from pathlib import Path
 
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score,
