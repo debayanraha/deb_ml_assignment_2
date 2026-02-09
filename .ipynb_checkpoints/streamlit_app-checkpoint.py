12146073beaa45971eec60c7d83d1a693590c9d1
Debayan Raha <debayan.raha@gmail.com>
2026-02-09 22:52:26 +0530

    ML models + Streamlit app

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -22,7 +22,7 @@ st.set_page_config(page_title="Mobile Price Classification", layout="wide")
 
 # Page refresh button at top
 col1, col2, col3 = st.columns([1, 1, 1])
-with col2:
+with col1:
     if st.button("🔄 Reset/Refresh App", type="primary"):
         st.rerun()
 
