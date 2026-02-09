ffa1e1674b1a250bf0017c26ce85f4413759cf14
Debayan Raha <debayan.raha@gmail.com>
2026-02-09 22:59:46 +0530

    ML models + Streamlit app

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import joblib
 from pathlib import Path
+from utils import train_model, predict
+
 
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score,
@@ -30,5 +32,44 @@ st.markdown("---")
 
 st.title("📱 Machine Learning Models (Mobile Price Classification)")
 
+# --------------------------------------------------
+# MODE SELECTION
+# --------------------------------------------------
+mode = st.radio(
+    "Choose Action",
+    ["Train a Model", "Predict a Model"]
+)
 
+models = [
+    "Logistic Regression",
+    "Decision Tree",
+    "KNN",
+    "Naive Bayes",
+    "Random Forest",
+    "XGBoost",
+]
 
+# --------------------------------------------------
+# TRAIN MODE
+# --------------------------------------------------
+if mode == "Train a Model":
+
+    st.subheader("Train a Machine Learning Model")
+
+    model_choice = st.selectbox(
+        "Select a Model to Train",
+        models,
+        index=None,
+        placeholder="Select a model"
+    )
+
+    if model_choice:
+        if st.button("🚀 Train Model"):
+            with st.spinner("Training model..."):
+                train_model(model_choice)
+            st.success(f"{model_choice} trained and saved successfully!")
+
+# --------------------------------------------------
+# PREDICT MODE
+# --------------------------------------------------
+else:
