fffea56029af83c03b88fc8f13e75781c3d17b72
Debayan Raha <debayan.raha@gmail.com>
2026-02-10 21:16:47 +0530

    ML models + Streamlit app

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -276,7 +276,7 @@ def perform_pre_processing(model_choice, df):
     # Create a copy for feature engineering
     df_engineered = df.copy()
     
-    if model_choice != "XGBoost"
+    if model_choice != "XGBoost":
         
         # 1. Screen Area (px_height * px_width)
         df_engineered['screen_area'] = df_engineered['px_height'] * df_engineered['px_width']
